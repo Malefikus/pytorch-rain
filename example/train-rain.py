405a26ec1f399e1a94a40d7c77c0bc1811b43ca9
Malefikus <leacy@live.cn>
2018-09-20 20:19:50 +0800

    epochs

--- example/train-rain.py
+++ example/train-rain.py
@@ -243,7 +243,7 @@ if __name__ == '__main__':
     # Training strategy
     parser.add_argument('-j', '--workers', default=1, type=int, metavar='N',
                         help='number of data loading workers (default: 4)')
-    parser.add_argument('--epochs', default=35, type=int, metavar='N',
+    parser.add_argument('--epochs', default=15, type=int, metavar='N',
                         help='number of total epochs to run')
     parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                         help='manual epoch number (useful on restarts)')
